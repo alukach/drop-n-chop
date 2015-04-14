1e971aa77c0a51fa585c431cf3ebce1ad622d836
Anthony Lukach <anthonylukach@gmail.com>
2015-04-13 22:56:10 -0600

    Add after-success step

---
+++ .travis/after_success.sh
@@ -0,0 +1,10 @@
+if [ "$TRAVIS_BRANCH" != "master" ]; then
+    exit 0;
+fi
+
+# export GIT_COMMITTER_EMAIL=...
+# export GIT_COMMITTER_NAME=...
+
+git checkout gh-pages || exit
+git merge "$TRAVIS_COMMIT" || exit
+git push ... # here need some authorization and url
