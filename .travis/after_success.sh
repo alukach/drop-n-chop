fdcb334cb95cb829f8bf8d132beb7b74a2c39920
Anthony Lukach <anthonylukach@gmail.com>
2015-04-14 21:23:04 -0600

    Add docstring, use variable for repo path

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,3 +1,15 @@
+# Post-build Deployment Script
+# Upon a successful build in the 'release branch', this script will set up Git
+# and the Grunt gh-pages command.
+#
+# This script requires that:
+#   - grunt-gh-pages be installed and configured: https://github.com/tschaub/grunt-gh-pages
+#   - a Github OAuth token with 'public_repo' permissions be added as a
+#     (non-displayed) environment variable within Travis, with the key 'GH_TOKEN'
+#   - the name of the branch to watch for commits and to deploy be added as an
+#     environment variable within Travis, with the key 'RELEASE_BRANCH'
+#
+
 if [ "$TRAVIS_BRANCH" != "$RELEASE_BRANCH" ]; then
     echo "In branch '$TRAVIS_BRANCH', not '$RELEASE_BRANCH'. Not deploying."
     exit 0;
@@ -6,8 +18,7 @@ fi
 echo "Configuring git..."
 git config --global user.email "travis@cugos.org"
 git config --global user.name "Travis CI"
-git config credential.helper "store --file=.git/credentials"
-git remote set-url origin https://${GH_TOKEN}@github.com/alukach/drop-n-chop.git
+git remote set-url origin https://${GH_TOKEN}@github.com/$TRAVIS_REPO_SLUG.git
 
 echo "Running gh-pages..."
 grunt gh-pages
